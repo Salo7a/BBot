eca67a6bc7784bf578c58422a07959269283741e
Salo7a <salo7a98x@gmail.com>
2020-11-19 21:45:03 +0200

    Playlist Updates #1

--- include/PlaylistSchema.js
+++ include/PlaylistSchema.js
@@ -8,6 +8,14 @@ const PlaylistSchema = new mongoose.Schema({
   Songs: {
     type: Array,
     required: [true, 'Songs Array is required']
+  },
+  SongsNames: {
+    type: Array,
+    required: [true, 'Songs Names Array is required']
+  },
+  SongsDuration: {
+    type: Array,
+    required: [true, 'Songs Names Array is required']
   }
 })
 

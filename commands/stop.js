0a8b0feac09e061478de1d6d9d7adee997db41de
Salo7a <salo7a98x@gmail.com>
2020-11-22 19:59:26 +0200

    Playlist Updates #8

--- commands/stop.js
+++ commands/stop.js
@@ -3,14 +3,16 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 module.exports = {
   name: "stop",
   description: "Stops the music",
-  execute(message) {
+  async execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-
+    const { channel } = message.member.voice;
     if (!queue) return message.reply("There is nothing playing.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     queue.songs = [];
-    queue.connection.dispatcher.end();
+    queue.connection.dispatcher.pause(true);
+    await queue.connection.dispatcher.end();
     queue.textChannel.send(`${message.author} ⏹ stopped the music!`).catch(console.error);
+    channel.leave();
   }
 };

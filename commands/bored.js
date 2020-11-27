703d042e4fb79dd8a8c014d2740c267ae070948a
Salo7a <salo7a98x@gmail.com>
2020-11-27 23:34:01 +0200

    BoredAPI Commands

---
+++ commands/bored.js
@@ -0,0 +1,26 @@
+const fetch = require("node-fetch");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "bored",
+  cooldown: 1,
+  aliases: ["bored"],
+  description: "Get a random thing to do",
+  async execute(message, args) {
+    fetch("https://www.boredapi.com/api/activity?participants=1")
+      .then(res => res.json())
+      .then(json => {
+        const embed = new MessageEmbed()
+          .setColor("#6BA3FF")
+          .setAuthor("Well, You Could", "https://i.imgur.com/7Y2F38n.png")
+          .setDescription(json.activity)
+          .setTimestamp();
+        message.channel.send(embed);
+        return;
+      })
+      .catch(err => {
+        message.channel.send("I have ran out of ideas :sob:");
+        return console.error(err);
+      });
+  }
+};

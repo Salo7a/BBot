c298a2122794be116d419e384bbf89d0f029cedd
Salo7a <salo7a98x@gmail.com>
2020-11-28 01:23:02 +0200

    Affirmations & Dad Jokes Commands

---
+++ commands/affirmation.js
@@ -0,0 +1,26 @@
+const fetch = require("node-fetch");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "affirmation",
+  cooldown: 1,
+  aliases: ["affirm", "sad", "matfi", "refa3y"],
+  description: "Contacts the spirits to get you an advice",
+  async execute(message, args) {
+    fetch("https://www.affirmations.dev/")
+      .then(res => res.json())
+      .then(json => {
+        const embed = new MessageEmbed()
+          .setColor("#403B3A")
+          .setAuthor("Sl7", "https://i.imgur.com/7524jhl.gif")
+          .setDescription(json.affirmation)
+          .setTimestamp();
+        message.channel.send(embed);
+        return;
+      })
+      .catch(err => {
+        message.channel.send("YOU CAN DO IT! :wink:");
+        return console.error(err);
+      });
+  }
+};

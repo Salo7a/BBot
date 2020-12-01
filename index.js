6edb6451dff41beca2d2e028a46ceef94e6c951a
Salo7a <salo7a98x@gmail.com>
2020-12-01 23:09:04 +0200

    Updates

--- index.js
+++ index.js
@@ -60,22 +60,22 @@ client.on("message", async (message) => {
   const now = Date.now();
   const timestamps = cooldowns.get(command.name);
   const cooldownAmount = (command.cooldown || 1) * 1000;
-
-  if (timestamps.has(message.author.id)) {
-    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-    if (now < expirationTime) {
-      const timeLeft = (expirationTime - now) / 1000;
-      return message.reply(
-        `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
-      );
-    }
-  }
-
-  timestamps.set(message.author.id, now);
   if (message.member.id != "324957055549177856"){
+    if (timestamps.has(message.author.id)) {
+      const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+      if (now < expirationTime) {
+        const timeLeft = (expirationTime - now) / 1000;
+        return message.reply(
+          `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
+        );
+      }
+    }
+
+    timestamps.set(message.author.id, now);
     setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
   }
+
   if (message.member.id == "579401365781086221"){
     await message.reply("Stupid Neko")
   } else if (message.member.id == "324957055549177856"){

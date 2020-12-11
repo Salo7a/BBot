b16817ac61f39d1cb7675b7bd624d6a4b4caeb75
Salo7a <salo7a98x@gmail.com>
2020-12-11 23:14:32 +0200

    Added Custom Per User Messages & ME Gif

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,7 @@ module.exports = {
   cooldown: 10,
   description: "Show the bot's average ping",
   execute(message) {
+    message.channel.send("https://tenor.com/view/mass-effect-liara-tsoni-gif-19524710").catch(console.error);
     message.reply(`📈 Average ping to API: ${Math.round(message.client.ws.ping)} ms`).catch(console.error);
   }
 };
--- util/EvobotUtil.js
+++ util/EvobotUtil.js
@@ -1,8 +1,15 @@
+let config;
+try {
+  config = require("../config.json");
+} catch (error) {
+  config = null;
+}
+let owner = config ? config.OWNER_ID: process.env.OWNER_ID;
+
 exports.canModifyQueue = (member) => {
   const { channelID } = member.voice;
   const botChannel = member.guild.voice.channelID;
-
-  if (channelID !== botChannel) {
+  if ((channelID !== botChannel) && member.id != owner) {
     member.send("You need to join the voice channel first!").catch(console.error);
     return;
   }
@@ -10,13 +17,6 @@ exports.canModifyQueue = (member) => {
   return true;
 };
 
-let config;
-
-try {
-  config = require("../config.json");
-} catch (error) {
-  config = null;
-}
 
 exports.TOKEN = config ? config.TOKEN : process.env.TOKEN;
 exports.PREFIX = config ? config.PREFIX : process.env.PREFIX;
@@ -28,3 +28,4 @@ exports.MONGODB_CONNECTION_STRING = config ? config.MONGODB_CONNECTION_STRING : 
 exports.STAY_TIME = config ? config.STAY_TIME : process.env.STAY_TIME;
 exports.TENOR_API = config ? config.TENOR_API : process.env.TENOR_API;
 exports.DEFAULT_VOLUME = config ? config.DEFAULT_VOLUME: process.env.DEFAULT_VOLUME;
+exports.OWNER_ID = config ? config.OWNER_ID: process.env.OWNER_ID;

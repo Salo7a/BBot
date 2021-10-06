2cf1de9075249721a207ae2f05077c0e0d428ec7
Salo7a <salo7a98x@gmail.com>
2021-10-06 03:16:46 +0200

    Updates

--- util/Util.js
+++ util/Util.js
@@ -1,5 +1,3 @@
-exports.canModifyQueue = (member) => member.voice.channelID === member.guild.voice.channelID;
-
 let config;
 
 try {
@@ -7,6 +5,10 @@ try {
 } catch (error) {
   config = null;
 }
+let owner = config ? config.OWNER_ID: process.env.OWNER_ID;
+
+exports.canModifyQueue = (member) => ((member.voice.channelID === member.guild.voice.channelID) || member.id === owner);
+
 
 exports.TOKEN = config ? config.TOKEN : process.env.TOKEN;
 exports.YOUTUBE_API_KEY = config ? config.YOUTUBE_API_KEY : process.env.YOUTUBE_API_KEY;
@@ -17,3 +19,6 @@ exports.PRUNING = (config ? config.PRUNING : process.env.PRUNING) || false;
 exports.STAY_TIME = (config ? config.STAY_TIME : process.env.STAY_TIME) || 30;
 exports.DEFAULT_VOLUME = (config ? config.DEFAULT_VOLUME : process.env.DEFAULT_VOLUME) || 100;
 exports.LOCALE = (config ? config.LOCALE : process.env.LOCALE) || "en";
+exports.MONGODB_CONNECTION_STRING = config ? config.MONGODB_CONNECTION_STRING : process.env.MONGODB_CONNECTION_STRING;
+exports.TENOR_API = config ? config.TENOR_API : process.env.TENOR_API;
+exports.OWNER_ID = config ? config.OWNER_ID: process.env.OWNER_ID;
